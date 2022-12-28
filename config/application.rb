3ab32f477c72985177e6490125f51117ed679c75
Thomas Conger <t.e.conger@gmail.com>
2022-12-28 10:42:07 -0800

    added user validation and seed file

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,6 @@ module LocusPocus
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore,
-      key: '_auth_me_session',
-      same_site: :lax,
-      secure: Rails.env.production?
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -41,5 +35,11 @@ module LocusPocus
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_auth_me_session',
+      same_site: :lax,
+      secure: Rails.env.production?
   end
 end
