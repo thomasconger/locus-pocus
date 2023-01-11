d22b5069eb68d06079e4f9963ed4aef6fbf51a63
Thomas Conger <t.e.conger@gmail.com>
2023-01-11 11:59:30 -0800

    adding stuff

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+
+    #Will I need an identifier? 
+    #identified_by :identifier
+
+    def connect
+      puts "Connection established!"
+    end
   end
 end
